14d679a254b4db5b0a39a5467dcf83fc9ff1836b
nueee <nue16@snu.ac.kr>
2021-12-04 22:09:29 +0900

    fixed loss function attributes

--- loss_functions.py
+++ loss_functions.py
@@ -12,22 +12,25 @@ class generatorLoss(nn.Module):
         self.w = w
         self.device = device
 
+        self.content_loss = 0.0
+        self.adversarial_loss = 0.0
+
         for param in self.feature_extractor.parameters():
             param.require_grad = False
 
     def forward(self, D_out_for_generated, G_in, G_out, current_epoch, image_size=256, tb_writer=None):
         if current_epoch < 10:
-            g_content_loss = self._content_loss(G_in, G_out)
-            g_adversarial_loss = 0.0
-            g_loss = g_content_loss
+            self.content_loss = self._content_loss(G_in, G_out)
+            self.adversarial_loss = 0.0
+            g_loss = self.content_loss
         else:
-            g_adversarial_loss = self._adversarial_loss_generator_part_only(D_out_for_generated, image_size)
-            g_content_loss = self._content_loss(G_in, G_out)
-            g_loss = g_adversarial_loss + self.w * g_content_loss
+            self.adversarial_loss = self._adversarial_loss_generator_part_only(D_out_for_generated, image_size)
+            self.content_loss = self._content_loss(G_in, G_out)
+            g_loss = self.adversarial_loss + self.w * self.content_loss
 
         if tb_writer:
-            tb_writer.add_scalar('g_adversarial_loss', g_adversarial_loss, current_epoch)
-            tb_writer.add_scalar('g_content_loss', g_content_loss, current_epoch)
+            tb_writer.add_scalar('g_adversarial_loss', self.adversarial_loss, current_epoch)
+            tb_writer.add_scalar('g_content_loss', self.content_loss, current_epoch)
             tb_writer.add_scalar('g_loss', g_loss, current_epoch)
 
         return g_loss
@@ -48,6 +51,10 @@ class discriminatorLoss(nn.Module):
         self.bce_loss = nn.BCELoss()
         self.device = device
 
+        self.adversarial_loss_cartoon = 0.0
+        self.adversarial_loss_smoothed = 0.0
+        self.adversarial_loss_G_input = 0.0
+
     def forward(self, D_out_for_cartoon, D_out_for_smoothed, D_out_for_generated, current_epoch, image_size=256, tb_writer=None):
 
         return self._adversarial_loss(D_out_for_cartoon, D_out_for_smoothed, D_out_for_generated, current_epoch, image_size, tb_writer)
@@ -57,16 +64,16 @@ class discriminatorLoss(nn.Module):
         target_zeros = zeros([actual_batch_size, 1, image_size//4, image_size//4]).to(self.device)
         target_ones = ones([actual_batch_size, 1, image_size//4, image_size//4]).to(self.device)
 
-        d_loss_cartoon = self.bce_loss(D_out_for_cartoon, target_ones)
-        d_loss_smoothed = self.bce_loss(D_out_for_smoothed, target_zeros)
-        d_loss_generated = self.bce_loss(D_out_for_generated, target_zeros)
+        self.adversarial_loss_cartoon = self.bce_loss(D_out_for_cartoon, target_ones)
+        self.adversarial_loss_smoothed = self.bce_loss(D_out_for_smoothed, target_zeros)
+        self.adversarial_loss_G_input = self.bce_loss(D_out_for_generated, target_zeros)
 
-        d_loss = d_loss_cartoon + d_loss_smoothed + d_loss_generated
+        d_loss = self.adversarial_loss_cartoon + self.adversarial_loss_smoothed + self.adversarial_loss_G_input
 
         if tb_writer:
-            tb_writer.add_scalar('d_loss_cartoon', d_loss_cartoon, current_epoch)
-            tb_writer.add_scalar('d_loss_smoothed', d_loss_smoothed, current_epoch)
-            tb_writer.add_scalar('d_loss_generated', d_loss_generated, current_epoch)
+            tb_writer.add_scalar('d_loss_cartoon', self.adversarial_loss_cartoon, current_epoch)
+            tb_writer.add_scalar('d_loss_smoothed', self.adversarial_loss_smoothed, current_epoch)
+            tb_writer.add_scalar('d_loss_generated', self.adversarial_loss_G_input, current_epoch)
             tb_writer.add_scalar('d_loss', d_loss, current_epoch)
 
         return d_loss
